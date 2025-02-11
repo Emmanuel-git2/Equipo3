86379c1821e6b0e74f2fafb0bdd2f9e2129c090b
aneth <aneth.mt@hotmail.com>
2025-02-11 00:19:36 -0600

    Se actualizan productos en categorias y se vincula correctamente con la dirección de las imagenes, se corrige ortografia de Formulario y Categorías

--- Categorias/CategoriasInteraccion.js
+++ Categorias/CategoriasInteraccion.js
@@ -64,75 +64,79 @@ function agregarProducto(item, containerId) {
 
 // Lista de productos para kids
 const productosKids = [
-    {name:'Alimentos', img:['ImagenesCategorias/FreeBuild0.jpg'] , description:'Bloque de construccion de libre constrccion de alimentos', price: 1000},
-    {name:'Hulk y Spiderman', img:'ImagenesCategorias/HulkSpiderman.jpg', description:'Set de personajes Hulk y Spiderman que incluyen vehiculos', price:2599},
-    {name:'Blanca Nieves', img:'ImagenesCategorias/BlancaNieves0.jpg', description:'Personajes de Blanca nieves y su casa', price: 3000},
-    {name:'Pepa Pig', img:'ImagenesCategorias/PepaPig0.jpg', description:'Personajes de pepa pig y su barco', price:1250},
-    {name:'Rey Leon Simba', img:'ImagenesCategorias/ReyLeon0.jpg', description:'Personaje Simba de la pelicula El Rey Leon', price:1680}
+    {name:'Alimentos', img:['ImagenesCategorias/FreeBuild0.jpg'] , description:'Bloques de construcción de libre construcción de alimentos', price: 1000},
+    {name:'Hulk y Spiderman', img:'ImagenesCategorias/HulkSpiderman.jpg', description:'Set de personajes Hulk y Spiderman que incluyen vehículos', price:2599},
+    {name:'Blanca Nieves', img:'ImagenesCategorias/BlancaNieves0.jpg', description:'Personajes de Blanca Nieves y su casa', price: 3000},
+    {name:'Peppa Pig', img:'ImagenesCategorias/PepaPig0.jpg', description:'Personajes de Peppa Pig y su barco', price:1250},
+    {name:'Rey León Simba', img:'ImagenesCategorias/ReyLeon0.jpg', description:'Personaje Simba de la película El Rey León', price:1680}
 ];
 
 // Lista de productos para adolescentes
 const productosTeens = [
-    {name: 'Harry Potter', img: 'ImagenesCategorias/HarryPotter0.jpg', description: 'Banco Magico Gringotts', price: 7000 },
-    {name: 'Personaje Fornite',img:'ImagenesCategorias/LlamaFornite0.jpg', description:'Construye la famosa llama del video juego Fornite', price:1900},
-    {name:'Santuario Doctor Strange',img:'ImagenesCategorias/MarvelDocStrange0.jpg', description:'Santuario Doctor Strange de peliculas Marvel', price:8599},
-    {name:'Samurai',img:'ImagenesCategorias/Samurai0.jpg', description:'Robot Samurai a escala con espada integrada', price:2999},
+    {name: 'Harry Potter', img: 'ImagenesCategorias/HarryPotter0.jpg', description: 'Banco mágico Gringotts', price: 7000 },
+    {name: 'Personaje Fortnite', img:'ImagenesCategorias/LlamaFornite0.jpg', description:'Construye la famosa llama del videojuego Fortnite', price:1900},
+    {name:'Santuario Doctor Strange', img:'ImagenesCategorias/MarvelDocStrange0.jpg', description:'Santuario Doctor Strange de películas Marvel', price:8599},
+    {name:'Samurái', img:'ImagenesCategorias/Samurai0.jpg', description:'Robot samurái a escala con espada integrada', price:2999},
     {name:'Batman', img:'ImagenesCategorias/Batman0.jpg', description:'Personaje Batman con motocicleta', price:2899}
 ];
 
 // Lista de productos para adultos
 const productosAdults = [
-    {name: 'Torre Paris', img:'ImagenesCategorias/TorreEiffel0.jpg', description: 'Torre de Paris para construir', price: 2200 },
-    {name: 'Titanic', img:'ImagenesCategorias/Titanic0.jpg', description:'Famoso barco Titatic para construir', price:4000},
-    {name:'Noche estrellada', img:'ImagenesCategorias/NocheEstrellada0.jpg', description:'Construye la gran pintura La noche estrellada de Vincent Van Gogh', price:2500},
-    {name:'Avion Concorde', img:'ImagenesCategorias/Concorde0.jpg', description:'Avion Supersonico Concorde', price:4900},
-    {name:'Automovil GT', img:'ImagenesCategorias/CarroAzul0.jpg', description:'Hermoso automovil a escala GT color azul', price:3800}
+    {name: 'Torre París', img:'ImagenesCategorias/TorreEiffel0.jpg', description: 'Torre de París para construir', price: 2200 },
+    {name: 'Titanic', img:'ImagenesCategorias/Titanic0.jpg', description:'Famoso barco Titanic para construir', price:4000},
+    {name:'Noche estrellada', img:'ImagenesCategorias/NocheEstrellada0.jpg', description:'Construye la gran pintura La noche estrellada de Vincent van Gogh', price:2500},
+    {name:'Avión Concorde', img:'ImagenesCategorias/Concorde0.jpg', description:'Avión supersónico Concorde', price:4900},
+    {name:'Automóvil GT', img:'ImagenesCategorias/CarroAzul0.jpg', description:'Hermoso automóvil a escala GT color azul', price:3800}
 ];
 
-// Lista de productos para Categoria > Nivel > Principiante
+// Lista de productos para Categoría > Nivel > Principiante
 const productosNivelJr = [
-    { name: 'Husky', img: 'PaginaInicio/imagenes/Husky.jpg', description: 'Bloques de construcción perro Husky', price: 200 },
-    { name: 'Uvas', img: 'PaginaInicio/imagenes/Uvas.jpg', description: 'Bloques de construcción uvas', price: 200 },
-    { name: 'Frutas', img: 'PaginaInicio/imagenes/KitFrutas.jpg', description: 'Bloques de construcción frutas', price: 168.99 },
-    { name: 'Kiwi', img: 'PaginaInicio/imagenes/Kiwi.jpg', description: 'Bloques de construcción de kiwi', price: 200 },
-    { name: 'Aguacate', img: 'PaginaInicio/imagenes/Aguacate.jpg', description: 'Bloques de construcción de aguacate', price: 200 },
-    { name: 'Pajarito Rojo', img: 'PaginaInicio/imagenes/PajaroRojo.jpg', description: 'Bloques de construcción de pajarito', price: 150 },
-    { name: 'Pajarito Azul y Blanco', img: 'PaginaInicio/imagenes/PajaroAzulConBlanco.jpg', description: 'Bloques de construcción de pajarito blanco con azul', price: 150 },
-    { name: 'Ballena', img: 'PaginaInicio/imagenes/BallenaBlanca.jpg', description: 'Bloques de construcción de ballena', price: 180 },
-    { name: 'Pingüino', img: 'PaginaInicio/imagenes/Pinguino.jpg', description: 'Bloques de construcción de pingüino', price: 200 },
-    { name: 'Cactus', img: 'PaginaInicio/imagenes/KitCactus.jpg', description: 'Bloques de construcción de cactus', price: 150 }
+    { name: 'Bambú', img: '../PaginaInicio/imagenes/Bambu.webp', description: 'Bambú de la suerte', price: 200.00 },
+    { name: 'Dumbo', img: '../PaginaInicio/imagenes/dumbo.webp', description: 'Dumbo de Disney', price: 280.00 },
+    { name: 'Ígor', img: '../PaginaInicio/imagenes/igor.webp', description: 'Ígor de Disney', price: 249.00 },
+    { name: 'Mei Panda Rojo', img: '../PaginaInicio/imagenes/panda.webp', description: 'Mei Panda Rojo de Disney', price: 367.00 },
+    { name: 'Gato', img: '../PaginaInicio/imagenes/gato.webp', description: 'Gato juguetón', price: 299.00 },
+    { name: 'Moto Yoshi', img: '../PaginaInicio/imagenes/motoYoshi.webp', description: 'Mario Kart: Moto Yoshi', price: 360.00 },
+    { name: 'Miniorquídea', img: '../PaginaInicio/imagenes/orquidea.webp', description: 'Miniorquídea color melón', price: 260.00 },
+    { name: 'Rosas', img: '../PaginaInicio/imagenes/rosas.webp', description: 'Rosas', price: 200.00 },
+    { name: 'Arreglo Floral', img: '../PaginaInicio/imagenes/arreglo.webp', description: 'Arreglo floral de varios colores', price: 450.00 },
+    { name: 'Ciruelo', img: '../PaginaInicio/imagenes/ciruelo.webp', description: 'Flor de ciruelo', price: 199.00 },
 ];
-// Lista de productos para Categoria > Nivel > Intermedio
+
+// Lista de productos para Categoría > Nivel > Intermedio
 const productosNivelMid = [
-    {name: 'Automovil', img: 'ImagenesCategorias/CarroVerdeOlivo0.jpg', description: 'Automovil color verde olivo a escala para construir, incluye conductor.', price: 300 },
-    {name: 'Browser Mario Bros', img:'ImagenesCategorias/Browser0.jpg', description:'Personaje Browser de Mario Bros', price:2589},
+    {name: 'Automóvil', img: 'ImagenesCategorias/CarroVerdeOlivo0.jpg', description: 'Automóvil color verde olivo a escala para construir, incluye conductor.', price: 300 },
+    {name: 'Bowser Mario Bros', img:'ImagenesCategorias/Browser0.jpg', description:'Personaje Bowser de Mario Bros', price:2589},
     {name:'Casco Piloto Star Wars', img:'ImagenesCategorias/CascoStarW0.jpg', description:'Casco de piloto de Star Wars', price: 4680},
 ];
 
-// Lista de productos para Categoria > Nivel > Avanzado
+// Lista de productos para Categoría > Nivel > Avanzado
 const productosNivelSenior = [
-    {name: 'Galaxia', img: 'ImagenesCategorias/Galaxia0.jpg', description: 'Galaxia VIa Lactea', price: 3200 },
+    {name: 'Galaxia', img: 'ImagenesCategorias/Galaxia0.jpg', description: 'Galaxia Vía Láctea', price: 3200 },
     {name: 'Castillo Harry Potter', img:'ImagenesCategorias/CastilloHarryPotter0.jpg', description:'Castillo de Harry Potter', price:5000},
-    {name:'Notre Dame', img:'ImagenesCategorias/NotreDame0.jpg', description:'Edificio Notre Dame de Paris', price:4669},
-    {name:'Star Wars Barcaza', img:'ImagenesCategorias/BarcazaStarWars0.jpg', description:'Barcaza de Velera de Jabba, Star WArs', price:5500},
-    {name:'Cohete NASA', img:'ImagenesCategorias/CoheteNASA0.jpg', description:'Sistema de lanzamiento espacial Artemis de NASA', price:3670}
+    {name:'Notre Dame', img:'ImagenesCategorias/NotreDame0.jpg', description:'Edificio Notre Dame de París', price:4669},
+    {name:'Star Wars Barcaza', img:'ImagenesCategorias/BarcazaStarWars0.jpg', description:'Barcaza velera de Jabba, Star Wars', price:5500},
+    {name:'Cohete NASA', img:'ImagenesCategorias/CoheteNASA0.jpg', description:'Sistema de lanzamiento espacial Artemis de la NASA', price:3670}
 ];
-// Lista de productos para Tematicas > Navidad
+
+// Lista de productos para Temáticas > Navidad
 const productosNavidad = [
-    {name: 'Pueblo Navidadeno', img: 'ImagenesCategorias/TownNavidad0.jpg', description: 'Set de Pueblo navideno', price: 3600 },
+    {name: 'Pueblo Navideño', img: 'ImagenesCategorias/TownNavidad0.jpg', description: 'Set de pueblo navideño', price: 3600 },
     {name: 'Esfera Santa', img:'ImagenesCategorias/EsferaSanta0.jpg', description:'Esfera de Santa Claus', price:500},
     {name:'Trineo', img:'ImagenesCategorias/TrineoSanta0.jpg', description:'Trineo de Santa Claus', price:1680},
-    {name:'Esfera Reno', img:'ImagenesCategorias/EsferaReno0.jpg', description:'Esfera de Reno de navidad', price:1000},
-    {name:'Arbol', img:'ImagenesCategorias/ArbolNavidad0.jpg', description:'Arbol navideno', price:1300}
+    {name:'Esfera Reno', img:'ImagenesCategorias/EsferaReno0.jpg', description:'Esfera de reno de Navidad', price:1000},
+    {name:'Árbol', img:'ImagenesCategorias/ArbolNavidad0.jpg', description:'Árbol navideño', price:1300}
 ];
-// Lista de productos para Tematicas > Amor y Amistad
+
+// Lista de productos para Temáticas > Amor y Amistad
 const productosAmorAmistad = [
-    { name: 'Rosas', img: 'ImagenesCategorias/Rosas0.jpg', description: 'Rosas para regalar y decorativas', price: 3300 },
+    { name: 'Rosas', img: 'ImagenesCategorias/Rosas0.jpg', description: 'Rosas para regalar y decorar', price: 3300 },
     {name: 'Arreglo floral', img:'ImagenesCategorias/FloresAmor0.jpg', description:'Construye un hermoso arreglo floral', price:2700},
     {name:'Love', img:'ImagenesCategorias/LOVE0.jpg', description:'Letras L O V E para construir y decorar', price:1090},
-    {name:'Amor de Oso', img:'ImagenesCategorias/OsosAmor0.jpg', description:'Osos con corazon', price:1400},
-    {name:'Piolin Cupido', img:'ImagenesCategorias/Piolin0.jpg', description:'Personaje Piolin en version cupido', price:3450}
+    {name:'Amor de Oso', img:'ImagenesCategorias/OsosAmor0.jpg', description:'Osos con corazón', price:1400},
+    {name:'Piolín Cupido', img:'ImagenesCategorias/Piolin0.jpg', description:'Personaje Piolín en versión cupido', price:3450}
 ];
+
 
 
 
